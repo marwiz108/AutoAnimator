42067c4f71d15044ff786b6f6329401e21252af3
Marwa Elali <marwaelali@gmail.com>
2021-04-05 16:35:43 +0100

    add canvas test file

---
+++ test/CanvasTest.java
@@ -0,0 +1,30 @@
+import model.canvas.Canvas;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CanvasTest {
+
+  private Canvas canvas;
+
+  @Before
+  public void setUp() {
+
+    this.canvas = new Canvas();
+  }
+
+  @Test
+  public void testAddShape() {
+  }
+
+  @Test
+  public void testAddTransformation() {
+  }
+
+  @Test
+  public void testGetShapesAtFrame() {
+  }
+
+  @Test
+  public void testToString() {
+  }
+}
