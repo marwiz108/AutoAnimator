1290612161d29aa17604a2005be79c82f55fc014
Marwa Elali <marwaelali@gmail.com>
2021-04-06 00:34:36 +0100

    include colour transformations in tests

--- test/CanvasTest.java
+++ test/CanvasTest.java
@@ -1,6 +1,6 @@
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
+import java.awt.Color;
 import model.canvas.Canvas;
 import model.shape.Oval;
 import model.shape.Point2D;
@@ -11,8 +11,8 @@ import model.transformation.ChangeVisibilityT;
 import model.transformation.MoveT;
 import model.transformation.ResizeT;
 import model.transformation.dimension;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
 
 public class CanvasTest {
 
@@ -21,7 +21,8 @@ public class CanvasTest {
   private Shape rectangle1;
   private ChangeVisibilityT changeRVis;
   private ChangeVisibilityT changeOVis;
-  private ChangeColorT changeColor;
+  private ChangeColorT changeRColor;
+  private ChangeColorT changeOColor;
   private MoveT move;
   private ResizeT resize;
 
@@ -40,7 +41,12 @@ public class CanvasTest {
             51, 153, 255);
     this.changeRVis = new ChangeVisibilityT(rectangle1, 5, 15);
     this.changeOVis = new ChangeVisibilityT(oval1, 0, 10);
-//    this.changeColor = new ChangeColorT()
+
+    this.changeRColor = new ChangeColorT(rectangle1, 7, 12,
+        this.rectangle1.getColor(), new Color(51, 153, 255));
+    this.changeOColor = new ChangeColorT(oval1, 2, 6,
+        this.oval1.getColor().getRed(), this.oval1.getColor().getGreen(),
+        this.oval1.getColor().getBlue(), 255, 255, 153);
 
     this.move = new MoveT(rectangle1, 10, 20, p2, p3);
     this.resize = new ResizeT(oval1, 15, 20, dimension.BASE,
@@ -133,6 +139,7 @@ public class CanvasTest {
     this.canvas.addShape(rectangle1);
     this.canvas.addShape(oval1);
     this.canvas.addTransformation("r", changeRVis);
+    this.canvas.addTransformation("r", changeRColor);
     this.canvas.addTransformation("r", move);
 
     assertEquals("Shapes:\n"
@@ -146,10 +153,12 @@ public class CanvasTest {
             + "Color: (255, 178, 102)\n\n"
             + "Transformations:\n"
             + "Shape r appears at t=5 and disappears at t=15\n"
+            + "Shape r changes color from (51, 153, 255) to (51, 153, 255) from t=7 to t=12\n"
             + "Shape r moves from (200.0, 80.0) to (25.0, 100.0) from t=10 to t=20\n",
         this.canvas.toString());
 
     this.canvas.addTransformation("o", changeOVis);
+    this.canvas.addTransformation("o", changeOColor);
     this.canvas.addTransformation("o", resize);
 
     assertEquals("Shapes:\n"
@@ -163,7 +172,9 @@ public class CanvasTest {
         + "Color: (255, 178, 102)\n\n"
         + "Transformations:\n"
         + "Shape o appears at t=0 and disappears at t=10\n"
+        + "Shape o changes color from (255, 178, 102) to (255, 255, 153) from t=2 to t=6\n"
         + "Shape r appears at t=5 and disappears at t=15\n"
+        + "Shape r changes color from (51, 153, 255) to (51, 153, 255) from t=7 to t=12\n"
         + "Shape r moves from (200.0, 80.0) to (25.0, 100.0) from t=10 to t=20\n"
         + "Shape o Scales from Base: 60.0, Height: 30.0 to Base: 80.0, " +
         "Height: 30.0 from t=15 to t=20\n", this.canvas.toString());
