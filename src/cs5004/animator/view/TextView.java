3a6214846e142b09cd11d89f90c7601f0259869c
Marwa Elali <marwaelali@gmail.com>
2021-04-13 00:17:47 +0100

    fill in textview interface

--- src/cs5004/animator/view/TextView.java
+++ src/cs5004/animator/view/TextView.java
@@ -1,3 +1,24 @@
 package cs5004.animator.view;
 
-public interface TextView {}
+/**
+ * This interface represents a Text view. The TextView is responsible
+ * for displaying the textual view and the SVG view of the animation.
+ */
+public interface TextView {
+
+  /**
+   * Returns the string representation of the text to be outputted.
+   * @return text to output
+   */
+  String generateText();
+
+  /**
+   * Creates the JFrame and JPanel for the view.
+   */
+  void createFile();
+
+  /**
+   * Renders the output of the view onto the file/window.
+   */
+  void render();
+}
