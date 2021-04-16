93cbee21792446c9edf47fce0d50561702a0bb65
murphyp <murphy.p@northeastern.edu>
2021-04-16 14:10:50 -0400

    Started trying to parse user input

--- src/cs5004/animator/EasyAnimator.java
+++ src/cs5004/animator/EasyAnimator.java
@@ -1,6 +1,8 @@
 package cs5004.animator;
 
+import java.io.Console;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import javax.swing.text.View;
 
@@ -12,12 +14,41 @@ import cs5004.animator.view.ViewFactoryImpl;
 
 public final class EasyAnimator {
   public static void main(String[] args) throws FileNotFoundException {
-    String inFile = "src/cs5004/animator/view/examples/buildings.txt";
-    String viewType = "visual";
-    int tps = 20;
-    int delay = 1000 / tps;
-    String outFile = "";
+    String inFile = null;
+    String viewType = null;
+    int delay = 1000;
+    String outFile = null;
 
+    Scanner sc = new Scanner(System.in);
+    while (sc.hasNext()) {
+      String input = sc.next();
+      System.out.println(input);
+      if (input.equals("-in")) {
+        inFile = "src/cs5004/animator/view/examples/" + sc.next();
+      } else if (input.equals("-view")) {
+        viewType = sc.next();
+      } else if (input.equals("-speed")) {
+        delay = 1000 / Integer.parseInt(sc.next());
+      } else if (input.equals("-out")) {
+        outFile = "src/cs5004/animator/view/outfiles" + sc.next();
+      } else {
+        throw new IllegalStateException("Invalid argument.");
+      }
+      System.out.print(inFile);
+      System.out.print(viewType);
+      System.out.print(delay);
+      System.out.println();
+    }
+    // -in buildings.txt -view visual -speed 24
+    //    inFile = "src/cs5004/animator/view/examples/buildings.txt";
+    //    viewType = "visual";
+    //    int tps = 20;
+    //    delay = 1000 / tps;
+    //    outFile = "";
+
+    if (inFile == null || viewType == null) {
+      throw new IllegalStateException("Required parameters not specified.");
+    }
     ICanvasModel.Builder builder = new ICanvasModel.Builder(inFile);
     ViewFactory factory = new ViewFactoryImpl(builder.getCanvas());
     factory.create(viewType, delay);
--- src/cs5004/animator/view/ViewFactory.java
+++ src/cs5004/animator/view/ViewFactory.java
@@ -1,8 +1,5 @@
 package cs5004.animator.view;
 
 public interface ViewFactory {
-
-  IView create(String viewType);
-
   IView create(String viewType, int delay);
 }
