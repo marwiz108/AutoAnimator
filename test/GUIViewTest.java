e2ef14f57ad9575e512849153e0eb6b19aae078e
Marwa Elali <marwaelali@gmail.com>
2021-04-26 00:50:03 +0100

    edit tests - all passing

--- test/GUIViewTest.java
+++ test/GUIViewTest.java
@@ -20,7 +20,7 @@ public class GUIViewTest {
   public void setUp() {
     builder = null;
     try {
-      builder = new ICanvasModel.Builder("resources/input/smalldemo.txt");
+      builder = new ICanvasModel.Builder("resources/input/toh-3.txt");
     } catch (FileNotFoundException | NullPointerException e) {
       System.out.println("Input file not found!");
       e.printStackTrace();
@@ -34,26 +34,49 @@ public class GUIViewTest {
   public void reset() {
     this.interactiveGUI.reset();
     assertEquals("Shapes:\n"
-        + "Name: R\n"
-        + "Type: rectangle\n"
-        + "Position: (200.0, 200.0), Base: 50.0, Height: 100.0\n"
-        + "Color: (255, 0, 0)\n"
-        + "\n"
-        + "Name: C\n"
-        + "Type: oval\n"
-        + "Position: (440.0, 70.0), Base: 120.0, Height: 60.0\n"
-        + "Color: (0, 0, 255)\n"
-        + "\n"
-        + "Transformations:\n"
-        + "Shape R appears at t=1.00 and disappears at t=100.00\n"
-        + "Shape C appears at t=6.00 and disappears at t=100.00\n"
-        + "Shape R moves from (200.0, 200.0) to (300.0, 300.0) from t=10.00 to t=50.00\n"
-        + "Shape C moves from (440.0, 70.0) to (440.0, 250.0) from t=20.00 to t=50.00\n"
-        + "Shape C changes color from (0, 0, 255) to (0, 170, 85) from t=50.00 to t=70.00\n"
-        + "Shape C moves from (440.0, 250.0) to (440.0, 370.0) from t=50.00 to t=70.00\n"
-        + "Shape R Scales from Base: 50.0, Height: 100.0 to Base: 25.0, Height: 100.0 from t=51.00 to t=70.00\n"
-        + "Shape C changes color from (0, 170, 85) to (0, 255, 0) from t=70.00 to t=80.00\n"
-        + "Shape R moves from (300.0, 300.0) to (200.0, 200.0) from t=70.00 to t=100.00\n",
+            + "Name: disk1\n"
+            + "Type: rectangle\n"
+            + "Position: (190.0, 180.0), Base: 20.0, Height: 30.0\n"
+            + "Color: (0, 49, 90)\n"
+            + "\n"
+            + "Name: disk2\n"
+            + "Type: rectangle\n"
+            + "Position: (167.0, 210.0), Base: 65.0, Height: 30.0\n"
+            + "Color: (6, 247, 41)\n"
+            + "\n"
+            + "Name: disk3\n"
+            + "Type: rectangle\n"
+            + "Position: (145.0, 240.0), Base: 110.0, Height: 30.0\n"
+            + "Color: (11, 45, 175)\n"
+            + "\n"
+            + "Transformations:\n"
+            + "Shape disk3 appears at t=1.00 and disappears at t=302.00\n"
+            + "Shape disk2 appears at t=1.00 and disappears at t=302.00\n"
+            + "Shape disk1 appears at t=1.00 and disappears at t=302.00\n"
+            + "Shape disk1 moves from (190.0, 180.0) to (190.0, 50.0) from t=25.00 to t=35.00\n"
+            + "Shape disk1 moves from (190.0, 50.0) to (490.0, 50.0) from t=36.00 to t=46.00\n"
+            + "Shape disk1 moves from (490.0, 50.0) to (490.0, 240.0) from t=47.00 to t=57.00\n"
+            + "Shape disk2 moves from (167.0, 210.0) to (167.0, 50.0) from t=57.00 to t=67.00\n"
+            + "Shape disk2 moves from (167.0, 50.0) to (317.0, 50.0) from t=68.00 to t=78.00\n"
+            + "Shape disk2 moves from (317.0, 50.0) to (317.0, 240.0) from t=79.00 to t=89.00\n"
+            + "Shape disk1 moves from (490.0, 240.0) to (490.0, 50.0) from t=89.00 to t=99.00\n"
+            + "Shape disk1 moves from (490.0, 50.0) to (340.0, 50.0) from t=100.00 to t=110.00\n"
+            + "Shape disk1 moves from (340.0, 50.0) to (340.0, 210.0) from t=111.00 to t=121.00\n"
+            + "Shape disk3 moves from (145.0, 240.0) to (145.0, 50.0) from t=121.00 to t=131.00\n"
+            + "Shape disk3 moves from (145.0, 50.0) to (445.0, 50.0) from t=132.00 to t=142.00\n"
+            + "Shape disk3 moves from (445.0, 50.0) to (445.0, 240.0) from t=143.00 to t=153.00\n"
+            + "Shape disk3 changes color from (11, 45, 175) to (0, 255, 0) from t=153.00 to t=161.00\n"
+            + "Shape disk1 moves from (340.0, 210.0) to (340.0, 50.0) from t=153.00 to t=163.00\n"
+            + "Shape disk1 moves from (340.0, 50.0) to (190.0, 50.0) from t=164.00 to t=174.00\n"
+            + "Shape disk1 moves from (190.0, 50.0) to (190.0, 240.0) from t=175.00 to t=185.00\n"
+            + "Shape disk2 moves from (317.0, 240.0) to (317.0, 50.0) from t=185.00 to t=195.00\n"
+            + "Shape disk2 moves from (317.0, 50.0) to (467.0, 50.0) from t=196.00 to t=206.00\n"
+            + "Shape disk2 moves from (467.0, 50.0) to (467.0, 210.0) from t=207.00 to t=217.00\n"
+            + "Shape disk2 changes color from (6, 247, 41) to (0, 255, 0) from t=217.00 to t=225.00\n"
+            + "Shape disk1 moves from (190.0, 240.0) to (190.0, 50.0) from t=217.00 to t=227.00\n"
+            + "Shape disk1 moves from (190.0, 50.0) to (490.0, 50.0) from t=228.00 to t=238.00\n"
+            + "Shape disk1 moves from (490.0, 50.0) to (490.0, 180.0) from t=239.00 to t=249.00\n"
+            + "Shape disk1 changes color from (0, 49, 90) to (0, 255, 0) from t=249.00 to t=257.00\n",
         builder.getCanvas().toString());
   }
 
@@ -64,11 +87,11 @@ public class GUIViewTest {
   @Test
   public void toggleLoop() {
     assertEquals("Animation Panel: Initial Delay=20 Current fps=50 Current Frame=0 "
-            + "Final Frame=100 Repeating=false Paused=false",
+            + "Final Frame=302 Repeating=false Paused=false",
         this.interactiveGUI.getAnimationPanel().toString());
     this.interactiveGUI.toggleLoop();
     assertEquals("Animation Panel: Initial Delay=20 Current fps=50 Current Frame=0 "
-            + "Final Frame=100 Repeating=true Paused=false",
+            + "Final Frame=302 Repeating=true Paused=false",
         this.interactiveGUI.getAnimationPanel().toString());
   }
 
@@ -87,11 +110,11 @@ public class GUIViewTest {
   @Test
   public void setSpeed() {
     assertEquals("Animation Panel: Initial Delay=20 Current fps=50 Current Frame=0 "
-            + "Final Frame=100 Repeating=false Paused=false",
+            + "Final Frame=302 Repeating=false Paused=false",
         this.interactiveGUI.getAnimationPanel().toString());
     this.interactiveGUI.setSpeed(80);
     assertEquals("Animation Panel: Initial Delay=20 Current fps=80 Current Frame=0 "
-            + "Final Frame=100 Repeating=false Paused=false",
+            + "Final Frame=302 Repeating=false Paused=false",
         this.interactiveGUI.getAnimationPanel().toString());
   }
 }
